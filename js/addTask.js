b53ea2dbbca2c11c5622650530e203d200857b40
fikrice <fikrice2025@gmail.com>
2025-05-12 23:39:38 +0700

    Menampilkan Data Task LocalStorage Part 2

--- js/addTask.js
+++ js/addTask.js
@@ -1,6 +1,12 @@
 // file ini digunakan untuk penghubung antara UI HTML dan model
 document.addEventListener('DOMContentLoaded', () => {
 
+    // membuat tanggal yang diharapkan  berformat 'yyyy-mm-dd'
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+
     const taskForm = document.getElementById('taskForm');
     const taskManager = new Task();
 
@@ -11,6 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const taskData = {
             taskName: document.getElementById('taskName').value,
             taskPriority: document.getElementById('taskPriority').value,
+            createdAt: `${year}-${month}-${day}`,
         };
 
         const result = taskManager.saveTask(taskData);
